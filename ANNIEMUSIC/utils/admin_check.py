20151401b8ba3783fd1567302358a97db5cf1b19
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-04-26 11:19:57 +0530

    Update admin_check.py

--- ANNIEMUSIC/utils/admin_check.py
+++ ANNIEMUSIC/utils/admin_check.py
@@ -1,6 +1,5 @@
-from pyrogram.enums import ChatMemberStatus, ChatType
 from pyrogram.types import Message
-
+from pyrogram.enums import ChatType, ChatMemberStatus
 
 async def is_admin(message: Message) -> bool:
     if not message.from_user:
@@ -16,16 +15,11 @@ async def is_admin(message: Message) -> bool:
     member = await client.get_chat_member(message.chat.id, message.from_user.id)
     return member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]
 
-
 async def is_group_owner(message: Message) -> bool:
     if not message.from_user:
         return False
 
-    if message.chat.type not in [
-        ChatType.SUPERGROUP,
-        ChatType.CHANNEL,
-        ChatType.GROUP,
-    ]:
+    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL, ChatType.GROUP,]:
         return False
 
     if message.from_user.id in [777000, 1087968824]:

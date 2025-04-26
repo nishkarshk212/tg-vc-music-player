c5ba2c630e50d5e983969a673a29c48c2c8fb650
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-04-26 11:23:42 +0530

    Update admin_check.py

--- ANNIEMUSIC/utils/admin_check.py
+++ ANNIEMUSIC/utils/admin_check.py
@@ -1,7 +1,12 @@
-from pyrogram.types import Message
+from pyrogram.types import Message, CallbackQuery
 from pyrogram.enums import ChatType, ChatMemberStatus
 
-async def is_admin(message: Message) -> bool:
+async def is_admin(message_or_cq) -> bool:
+    if isinstance(message_or_cq, CallbackQuery):
+        message = message_or_cq.message
+    else:
+        message = message_or_cq
+
     if not message.from_user:
         return False
 
@@ -15,11 +20,16 @@ async def is_admin(message: Message) -> bool:
     member = await client.get_chat_member(message.chat.id, message.from_user.id)
     return member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]
 
-async def is_group_owner(message: Message) -> bool:
+async def is_group_owner(message_or_cq) -> bool:
+    if isinstance(message_or_cq, CallbackQuery):
+        message = message_or_cq.message
+    else:
+        message = message_or_cq
+
     if not message.from_user:
         return False
 
-    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL, ChatType.GROUP,]:
+    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL, ChatType.GROUP]:
         return False
 
     if message.from_user.id in [777000, 1087968824]:

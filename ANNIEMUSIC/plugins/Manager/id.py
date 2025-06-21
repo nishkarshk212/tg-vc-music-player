81613dc01626cd5992cca34631482de284a9e513
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-06-21 12:57:15 +0530

    Fix Markdown parsing .

--- ANNIEMUSIC/plugins/Manager/id.py
+++ ANNIEMUSIC/plugins/Manager/id.py
@@ -5,41 +5,54 @@ from pyrogram.types import Message
 from ANNIEMUSIC import app
 
 
-@app.on_message(filters.command('id'))
+@app.on_message(filters.command("id"))
 async def get_id(client, message: Message):
-    chat = message.chat
-    user = message.from_user
-    reply = message.reply_to_message
+    chat, user, reply = message.chat, message.from_user, message.reply_to_message
+    out = []
 
-    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message.id}`\n"
-    text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={user.id})** `{user.id}`\n"
+    if message.link:
+        out.append(f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message.id}`")
+    else:
+        out.append(f"**ᴍᴇssᴀɢᴇ ɪᴅ:** `{message.id}`")
+
+    out.append(f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={user.id})** `{user.id}`")
 
     if len(message.command) == 2:
         try:
-            target = message.text.split(None, 1)[1].strip()
-            target_user = await client.get_users(target)
-            text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={target_user.id})** `{target_user.id}`\n"
+            target = message.text.split(maxsplit=1)[1]
+            tgt_user = await client.get_users(target)
+            out.append(f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={tgt_user.id})** `{tgt_user.id}`")
         except Exception:
             return await message.reply_text("**ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.**", quote=True)
 
-    if chat.username:
-        text += f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+    if chat.username and chat.type != "private":
+        out.append(f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`")
     else:
-        text += f"**ᴄʜᴀᴛ ɪᴅ:** `{chat.id}`\n\n"
+        out.append(f"**ᴄʜᴀᴛ ɪᴅ:** `{chat.id}`")
 
     if reply:
+        if reply.link:
+            out.append(f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`")
+        else:
+            out.append(f"**ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:** `{reply.id}`")
+
         if reply.from_user:
-            text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
-            text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+            out.append(
+                f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** "
+                f"`{reply.from_user.id}`"
+            )
 
         if reply.forward_from_chat:
-            text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
+            out.append(
+                f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ **{reply.forward_from_chat.title}** "
+                f"ʜᴀs ɪᴅ `{reply.forward_from_chat.id}`"
+            )
 
         if reply.sender_chat:
-            text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ: `{reply.sender_chat.id}`"
+            out.append(f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ: `{reply.sender_chat.id}`")
 
     await message.reply_text(
-        text,
+        "\n".join(out),
         disable_web_page_preview=True,
-        parse_mode=ParseMode.DEFAULT
+        parse_mode=ParseMode.MARKDOWN,
     )

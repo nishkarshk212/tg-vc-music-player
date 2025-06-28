be4a02d9fd9815ec33661f817eff30c21c9e32ed
Certified Coder <155803358+CertifiedCoders@users.noreply.github.com>
2025-06-28 11:52:15 +0530

    Update bot.py

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -15,6 +15,7 @@ class JARVIS(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
+            in_memory=True,
             workers=30,
             max_concurrent_transmissions=7,
         )
